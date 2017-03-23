787aa344fd0a2bc9bdbf84c9b6d2edf7f34c91be
Monkey Liang <i@aq741.com>
2017-03-23 22:38:08 +0800

    MPI HelloWorld

--- MPIHello/main.c
+++ MPIHello/main.c
@@ -3,8 +3,12 @@
 
 int main(int argc, char *argv[])
 {
+    int comm_sz;
+    int my_rank;
     MPI_Init(&argc, &argv);
-    printf("Hello World!\n");
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    printf("Hello World from process %d of %d!\n", my_rank, comm_sz);
     MPI_Finalize();
     return 0;
 }
